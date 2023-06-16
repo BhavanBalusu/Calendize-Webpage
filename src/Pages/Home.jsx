d255a69d1d0c596522b478f20e6f0bb35c1bbb35
anuragchillarige <pinganurag@gmail.com>
2023-06-16 16:49:16 -0700

    finished settings page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,27 +4,27 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db, logout } from "../firebase";
 import TypeAnimation from 'react-type-animation';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 export default function Home() {
     const nav = useNavigate();
     const [currUser] = useAuthState(auth);
 
-    useEffect( ()=>{
-        if (currUser){
-            nav('/dashboard')
+    useEffect(() => {
+        if (currUser) {
+            nav('/dash')
         }
-    },[currUser])
+    }, [currUser])
 
     return (
         <div className="home">
             <nav className="home-nav">
                 <img src={logo} alt="calendize logo" />
                 <div style={{ "width": "70%" }}></div>
-                <button className="sign-button sign-in" onClick={e => { e.preventDefault(); logout(); window.open("http://localhost:3000/sign","_blank") }}>Log In</button>
+                <button className="sign-button sign-in" onClick={e => { e.preventDefault(); logout(); window.open("http://localhost:3000/sign", "_blank") }}>Log In</button>
                 <a target="_blank">
-                    <button className="sign-button sign-up" onClick={e => { e.preventDefault(); logout(); window.open("http://localhost:3000/register","_blank") }}>Sign Up</button>
+                    <button className="sign-button sign-up" onClick={e => { e.preventDefault(); logout(); window.open("http://localhost:3000/register", "_blank") }}>Sign Up</button>
                 </a>
             </nav>
 
